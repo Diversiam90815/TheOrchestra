343ac6c67f5b3d9d51eaed7b65e638b3eac76857
Jens Langenberg <Jens.Langenberg@corsair.com>
2024-06-15 20:12:51 +0200

    Added documentation part 2; Cleaned up public code in classes

--- Source/CustomPianoRoll.h
+++ Source/CustomPianoRoll.h
@@ -15,7 +15,7 @@
 #include "Parameters.h"
 
 
-//Enum to identify the key used
+// Enum to identify the key used
 enum key
 {
 	defaultKey = 0,
@@ -30,73 +30,80 @@ enum key
 class CustomPianoRoll : public MidiKeyboardComponent
 {
 public:
-    //==============================================================================
-    //                          CONSTRUCTOR - DESTRUCTOR
-    //==============================================================================
+	//==============================================================================
+	//                          CONSTRUCTOR - DESTRUCTOR
+	//==============================================================================
 
 	CustomPianoRoll(MidiKeyboardState &state, Orientation orientation);
 
-    ~CustomPianoRoll() = default;
-    
-    
-    //==============================================================================
-    //                          PUBLIC METHODS
-    //==============================================================================
+	~CustomPianoRoll() = default;
 
-	void	   drawWhiteNote(int midiNoteNumber, Graphics &g, Rectangle<float> area, bool isDown, bool isOver, Colour lineColour, Colour textColour) override;
 
-	void	   drawBlackNote(int midiNoteNumber, Graphics &g, Rectangle<float> area, bool isDown, bool isOver, Colour noteFillColour) override;
+	//==============================================================================
+	//                          PUBLIC METHODS
+	//==============================================================================
 
-    /*
-     @brief                 Turns the note name in accordance with the octave in form of E4, C3,.. to the corresponding midi note value.                          Middle C (C4) corresponds to a midi note value of 60.
-     @param                 [IN] notename -> The name of the note and the octave -> E4, C6, C#2, Gb3,...
-     @return                Midi note value as an int of the note given. If there an error occured, it returns -1.
-    */
+
+	/*
+	 @brief                 Turns the note name in accordance with the octave in form of E4, C3,.. to the corresponding midi note value.                          Middle C (C4)
+	 corresponds to a midi note value of 60.
+	 @param                 [IN] notename -> The name of the note and the octave -> E4, C6, C#2, Gb3,...
+	 @return                Midi note value as an int of the note given. If there an error occured, it returns -1.
+	*/
 	static int turnNotenameIntoMidinumber(String notename);
 
-    /*
-     @brief                 Fills the vector 'mMidiRanges' with the note ranges information from a StringArray
-     @param                 [IN] qualities as an StringArray.
-     @return                Boolean indicating success/failure
-    */
+	/*
+	 @brief                 Fills the vector 'mMidiRanges' with the note ranges information from a StringArray
+	 @param                 [IN] qualities as an StringArray.
+	 @return                Boolean indicating success/failure
+	*/
 	bool	   setMidiRanges(const StringArray &qualities);
 
-    /*
-     @brief                 Fills the vector 'mMidiRanges' with the note ranges information from a String
-     @param                 [IN] ranges as an String.
-     @return                Boolean indicating success/failure
-    */
+	/*
+	 @brief                 Fills the vector 'mMidiRanges' with the note ranges information from a String
+	 @param                 [IN] ranges as an String.
+	 @return                Boolean indicating success/failure
+	*/
 	bool	   setMidiRanges(const String &ranges);
 
-    
-private:
-    //==============================================================================
-    //                          PRIVATE METHODS
-    //==============================================================================
 
-    /*
-     @brief                 Returns the note color depending on the note midi value
-     @param                 [IN] midiNoteNumber -> midi note value of the note (C4 = 60)
-     @return                Colour of the note that should be used
-    */
+private:
+	//==============================================================================
+	//                          PRIVATE METHODS
+	//==============================================================================
+
+	/*
+	 @brief                 Returns the note color depending on the note midi value
+	 @param                 [IN] midiNoteNumber -> midi note value of the note (C4 = 60)
+	 @return                Colour of the note that should be used
+	*/
 	Colour							 getNoteColour(int midiNoteNumber);
 
-    
-    //==============================================================================
-    //                          PRIVATE OBJECTS
-    //==============================================================================
+	/*
+	 @brief                 Override JUCE function to paint the white notes
+	*/
+	void							 drawWhiteNote(int midiNoteNumber, Graphics &g, Rectangle<float> area, bool isDown, bool isOver, Colour lineColour, Colour textColour) override;
 
-	std::vector<std::pair<int, int>> mMidiRanges;           //Vector of the ranges (stored as pairs of int). This is used to draw different                                                          colours of notes
+	/*
+	 @brief                 Override JUCE function to paint the black notes
+	*/
+	void							 drawBlackNote(int midiNoteNumber, Graphics &g, Rectangle<float> area, bool isDown, bool isOver, Colour noteFillColour) override;
 
-	Atomic<bool>					 mRangesSet		 = false;       //If true, the custom piano roll is being applied. This is set to true, if                                                               mMidiRanges will be filled
 
-	Atomic<int>						 mCurrentKeyType = key::defaultKey;     //Indicating the current key type for drawing
-    
-	std::vector<Colour>				 qualityColours	 =
-    {
-		  Colours::lightblue.darker(), // For the first range
-		  Colours::lightgreen,		   // For the second range
-		  Colours::lightcoral,		   // For the third range
-		  Colours::gold				   // Additional ranges, etc.
-    };                                                              //Different colours used for the ranges of the different qualities
+	//==============================================================================
+	//                          PRIVATE OBJECTS
+	//==============================================================================
+
+	std::vector<std::pair<int, int>> mMidiRanges;						// Vector of the ranges (stored as pairs of int). This is used to draw different colours of notes
+
+	Atomic<bool>					 mRangesSet		 = false;			// If true, the custom piano roll is being applied. This is set to true, if mMidiRanges will be filled
+
+	Atomic<int>						 mCurrentKeyType = key::defaultKey; // Indicating the current key type for drawing
+
+	std::vector<Colour>				 qualityColours	 = {
+		  Colours::lightblue.darker(),						// For the first range
+		  Colours::lightgreen,								// For the second range
+		  Colours::lightcoral,								// For the third range
+		  Colours::gold										// Additional ranges, etc.
+	  };													// Different colours used for the ranges of the different qualities
 };
