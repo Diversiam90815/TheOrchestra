1310ddd1032c26f3dd1b66f921e44da347929eb4
Diversiam <jerry2912@icloud.com>
2024-11-16 17:00:26 +0100

    Removed Sampling Feature due to future refactor

--- src/InstrumentInfo/InstrumentInfoView.cpp
+++ src/InstrumentInfo/InstrumentInfoView.cpp
@@ -160,7 +160,7 @@ void InstrumentInfoView::menuItemSelected(int menuItemID, int topLevelMenuIndex)
 {
 	int key = (topLevelMenuIndex + 1) * 100 + menuItemID;
 	showInstrumentInfo(key);
-	mOrchestraSampler.addSoundsFromInstrumentToSampler(key);
+	//mOrchestraSampler.addSoundsFromInstrumentToSampler(key);
 }
 
 
