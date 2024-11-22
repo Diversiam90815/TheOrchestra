0b31d39875d80dec53131a9c9a9c102b4621257a
Diversiam <jerry2912@icloud.com>
2024-11-22 18:35:23 +0100

    Laid out basic UI

--- src/Views/InstrumentRangesView.cpp
+++ src/Views/InstrumentRangesView.cpp
@@ -25,15 +25,25 @@ void InstrumentRangesView::init()
 	setLookAndFeel(&mCustomLookAndFeel);
 	setSize(mWidth, mHeight);
 
+	// Set the Ranges title
 	mRangesTitle.setName("RangesTitle");
 	mRangesTitle.setText("Ranges", NotificationType::dontSendNotification);
+	mRangesTitle.setBounds(mRangesTitleX, mRangesTitleY, mRangesTitleWidth, mRangesTitleHeight);
 
+	// Set the Transposition title
 	mTranspositionTitle.setName("TranspositionTitle");
 	mTranspositionTitle.setText("Transposition", NotificationType::dontSendNotification);
+	mTranspositionTitle.setBounds(mTranspositionTitleX, mTranspositionTitleY, mTranspositionTitleWidth, mTranspositionTitleHeight);
 
+	// Set the images positions
 	mLowerRangeNoteImage.setBounds(mLowerNoteImageX, mLowerNoteImageY, mImageBoxWidth, mImageBoxHeight);
 	mHigherRangeNoteImage.setBounds(mHigherNoteImageX, mHigherNoteImageY, mImageBoxWidth, mImageBoxHeight);
 	mTranspositionNoteImage.setBounds(mTranspositionImageX, mTranspositionImageY, mImageBoxWidth, mImageBoxHeight);
+
+	// Set the note name's positions
+	mLowerRangeNoteName.setBounds(mLowerNoteNameX, mLowerNoteNameY, mNoteNameWidth, mNoteNameHeight);
+	mHigherRangeNoteName.setBounds(mHigherNoteNameX, mHigherNoteNameY, mNoteNameWidth, mNoteNameHeight);
+	mTranspositionNoteName.setBounds(mTranspositionNoteX, mTranspositionNoteY, mTranspositionNoteWidth, mTranspositionNoteHeight);
 }
 
 
--- src/Views/QualitiesView.cpp
+++ src/Views/QualitiesView.cpp
@@ -24,6 +24,30 @@ void QualitiesView::init()
 {
 	setLookAndFeel(&mCustomLookAndFeel);
 	setSize(mWidth, mHeight);
+
+	// Set the title
+	mTitle.setName("QualitiesTitle");
+	mTitle.setText("Qualities", dontSendNotification);
+	mTitle.setBounds(mQualitiesTitleX, mQualitiesTitleY, mQualitiesTitleWidth, mQualitiesTitleHeight);
+
+	// Settings the note names positions
+	mFirstLowerNote.setBounds(mLowerNoteX, mFirstRowY, mNoteLabelWidth, mNoteLabelHeight);
+	mFirstHigherNote.setBounds(mHigherNoteX, mFirstRowY, mNoteLabelWidth, mNoteLabelHeight);
+
+	mSecondLowerNote.setBounds(mLowerNoteX, mSecondRowY, mNoteLabelWidth, mNoteLabelHeight);
+	mSecondLowerNote.setBounds(mHigherNoteX, mSecondRowY, mNoteLabelWidth, mNoteLabelHeight);
+
+	mThirdLowerNote.setBounds(mLowerNoteX, mThirdRowY, mNoteLabelWidth, mNoteLabelHeight);
+	mThirdHigherNote.setBounds(mHigherNoteX, mThirdRowY, mNoteLabelWidth, mNoteLabelHeight);
+
+	mFourthLowerNote.setBounds(mLowerNoteX, mFourthRowY, mNoteLabelWidth, mNoteLabelHeight);
+	mFourthLowerNote.setBounds(mHigherNoteX, mFourthRowY, mNoteLabelWidth, mNoteLabelHeight);
+
+	// Setting the qualities positions
+	mFirstQuality.setBounds(mQualityX, mFirstRowY, mQualityWidth, mQualityHeight);
+	mSecondQuality.setBounds(mQualityX, mSecondRowY, mQualityWidth, mQualityHeight);
+	mThirdQuality.setBounds(mQualityX, mThirdRowY, mQualityWidth, mQualityHeight);
+	mFourthQuality.setBounds(mQualityX, mFourthRowY, mQualityWidth, mQualityHeight);
 }
 
 
