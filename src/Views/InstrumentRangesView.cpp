6705b3c8a905886119095f13e3e3d530cc7b7453
Diversiam <jerry2912@icloud.com>
2024-11-22 19:01:47 +0100

    Basic layout now complete

--- src/Views/InstrumentRangesView.cpp
+++ src/Views/InstrumentRangesView.cpp
@@ -26,23 +26,35 @@ void InstrumentRangesView::init()
 	setSize(mWidth, mHeight);
 
 	// Set the Ranges title
+	addAndMakeVisible(&mRangesTitle);
 	mRangesTitle.setName("RangesTitle");
 	mRangesTitle.setText("Ranges", NotificationType::dontSendNotification);
 	mRangesTitle.setBounds(mRangesTitleX, mRangesTitleY, mRangesTitleWidth, mRangesTitleHeight);
 
 	// Set the Transposition title
+	addAndMakeVisible(&mTranspositionTitle);
 	mTranspositionTitle.setName("TranspositionTitle");
 	mTranspositionTitle.setText("Transposition", NotificationType::dontSendNotification);
 	mTranspositionTitle.setBounds(mTranspositionTitleX, mTranspositionTitleY, mTranspositionTitleWidth, mTranspositionTitleHeight);
 
 	// Set the images positions
+	addAndMakeVisible(&mLowerRangeNoteImage);
 	mLowerRangeNoteImage.setBounds(mLowerNoteImageX, mLowerNoteImageY, mImageBoxWidth, mImageBoxHeight);
+	
+	addAndMakeVisible(&mHigherRangeNoteImage);
 	mHigherRangeNoteImage.setBounds(mHigherNoteImageX, mHigherNoteImageY, mImageBoxWidth, mImageBoxHeight);
+
+	addAndMakeVisible(&mTranspositionNoteImage);
 	mTranspositionNoteImage.setBounds(mTranspositionImageX, mTranspositionImageY, mImageBoxWidth, mImageBoxHeight);
 
 	// Set the note name's positions
+	addAndMakeVisible(&mLowerRangeNoteName);
 	mLowerRangeNoteName.setBounds(mLowerNoteNameX, mLowerNoteNameY, mNoteNameWidth, mNoteNameHeight);
+	
+	addAndMakeVisible(&mHigherRangeNoteName);
 	mHigherRangeNoteName.setBounds(mHigherNoteNameX, mHigherNoteNameY, mNoteNameWidth, mNoteNameHeight);
+	
+	addAndMakeVisible(&mTranspositionNoteName);
 	mTranspositionNoteName.setBounds(mTranspositionNoteX, mTranspositionNoteY, mTranspositionNoteWidth, mTranspositionNoteHeight);
 }
 
