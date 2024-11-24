ffd054e4014a4d9569b08a6fd42117bc7f5d5a1d
Diversiam <jerry2912@icloud.com>
2024-11-24 13:40:02 +0100

    Added button colours

--- src/Views/CustomLookAndFeel.cpp
+++ src/Views/CustomLookAndFeel.cpp
@@ -52,6 +52,7 @@ void CustomLookAndFeel::setFont()
 	// Load the custom typeface from BinaryData
 	instrumentTypeface = Typeface::createSystemTypefaceFor(BinaryData::InstrumentSerifRegular_ttf, BinaryData::InstrumentSerifRegular_ttfSize);
 
+	// Set the font
 	headerFont		   = Font(instrumentTypeface).withHeight(24.0f);
 	noteNameFonts	   = Font(instrumentTypeface).withHeight(24.0f);
 	menuFont		   = Font(instrumentTypeface).withHeight(24.0f);
@@ -83,7 +84,6 @@ void CustomLookAndFeel::drawLabel(Graphics &g, Label &label)
 void CustomLookAndFeel::drawMenuBarBackground(Graphics &g, int width, int height, bool isMouseOverBar, MenuBarComponent &menuBar)
 {
 	g.fillAll(menuBarBackground);
-
 	g.fillRect(0, 0, width, height);
 }
 
@@ -114,4 +114,42 @@ void CustomLookAndFeel::drawPopupMenuItem(Graphics			   &g,
 	g.setColour(isHighlighted ? findColour(PopupMenu::highlightedTextColourId) : findColour(PopupMenu::textColourId));
 	g.setFont(getPopupMenuFont());
 	g.drawText(text, area.reduced(10, 0), Justification::centred, true);
+}
+
+
+void CustomLookAndFeel::drawButtonBackground(Graphics &g, Button &button, const Colour &backgroundColour, bool shouldDrawButtonAsHighlighted, bool shouldDrawButtonAsDown)
+{
+	auto cornerSize = 6.0f;
+	auto bounds		= button.getLocalBounds().toFloat().reduced(0.5f, 0.5f);
+
+	//auto baseColour = backgroundColour.withMultipliedSaturation(button.hasKeyboardFocus(true) ? 1.3f : 0.9f).withMultipliedAlpha(button.isEnabled() ? 1.0f : 0.5f);
+
+	//if (shouldDrawButtonAsDown || shouldDrawButtonAsHighlighted)
+	//	baseColour = baseColour.contrasting(shouldDrawButtonAsDown ? 0.2f : 0.05f);
+
+	g.setColour(techniqueButtonColour);
+
+	auto flatOnLeft	  = button.isConnectedOnLeft();
+	auto flatOnRight  = button.isConnectedOnRight();
+	auto flatOnTop	  = button.isConnectedOnTop();
+	auto flatOnBottom = button.isConnectedOnBottom();
+
+	if (flatOnLeft || flatOnRight || flatOnTop || flatOnBottom)
+	{
+		Path path;
+		path.addRoundedRectangle(bounds.getX(), bounds.getY(), bounds.getWidth(), bounds.getHeight(), cornerSize, cornerSize, !(flatOnLeft || flatOnTop),
+								 !(flatOnRight || flatOnTop), !(flatOnLeft || flatOnBottom), !(flatOnRight || flatOnBottom));
+
+		g.fillPath(path);
+
+		g.setColour(button.findColour(ComboBox::outlineColourId));
+		g.strokePath(path, PathStrokeType(1.0f));
+	}
+	else
+	{
+		g.fillRoundedRectangle(bounds, cornerSize);
+
+		g.setColour(button.findColour(ComboBox::outlineColourId));
+		g.drawRoundedRectangle(bounds, cornerSize, 1.0f);
+	}
 }
