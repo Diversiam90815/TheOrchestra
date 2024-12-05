0f9dcaac1d5185097647b8a8c902fb71436c3753
Diversiam <jerry2912@icloud.com>
2024-12-05 20:49:26 +0100

    Qualities now get displayed

--- src/Views/QualitiesView.cpp
+++ src/Views/QualitiesView.cpp
@@ -32,24 +32,37 @@ void QualitiesView::init()
 
 	// Settings the note names positions
 	addAndMakeVisible(&mFirstLowerNote);
-	mFirstLowerNote.setBounds(mLowerNoteX, mFirstRowY, mNoteLabelWidth, mNoteLabelHeight);
+	mFirstLowerNote.setBounds(mLowerNoteX, mFirstRowY, mNoteButtonWidth, mNoteButtonHeight);
+	mFirstLowerNote.setConnectedEdges(Button::ConnectedOnRight);
+
 	addAndMakeVisible(&mFirstHigherNote);
-	mFirstHigherNote.setBounds(mHigherNoteX, mFirstRowY, mNoteLabelWidth, mNoteLabelHeight);
+	mFirstHigherNote.setBounds(mHigherNoteX, mFirstRowY, mNoteButtonWidth, mNoteButtonHeight);
+	mFirstHigherNote.setConnectedEdges(Button::ConnectedOnLeft);
 
 	addAndMakeVisible(&mSecondLowerNote);
-	mSecondLowerNote.setBounds(mLowerNoteX, mSecondRowY, mNoteLabelWidth, mNoteLabelHeight);
+	mSecondLowerNote.setBounds(mLowerNoteX, mSecondRowY, mNoteButtonWidth, mNoteButtonHeight);
+	mSecondLowerNote.setConnectedEdges(Button::ConnectedOnRight);
+
 	addAndMakeVisible(&mSecondHigherNote);
-	mSecondHigherNote.setBounds(mHigherNoteX, mSecondRowY, mNoteLabelWidth, mNoteLabelHeight);
+	mSecondHigherNote.setBounds(mHigherNoteX, mSecondRowY, mNoteButtonWidth, mNoteButtonHeight);
+	mSecondHigherNote.setConnectedEdges(Button::ConnectedOnLeft);
 
 	addAndMakeVisible(&mThirdLowerNote);
-	mThirdLowerNote.setBounds(mLowerNoteX, mThirdRowY, mNoteLabelWidth, mNoteLabelHeight);
+	mThirdLowerNote.setBounds(mLowerNoteX, mThirdRowY, mNoteButtonWidth, mNoteButtonHeight);
+	mThirdLowerNote.setConnectedEdges(Button::ConnectedOnRight);
+
 	addAndMakeVisible(&mThirdHigherNote);
-	mThirdHigherNote.setBounds(mHigherNoteX, mThirdRowY, mNoteLabelWidth, mNoteLabelHeight);
+	mThirdHigherNote.setBounds(mHigherNoteX, mThirdRowY, mNoteButtonWidth, mNoteButtonHeight);
+	mThirdHigherNote.setConnectedEdges(Button::ConnectedOnLeft);
 
 	addAndMakeVisible(&mFourthLowerNote);
-	mFourthLowerNote.setBounds(mLowerNoteX, mFourthRowY, mNoteLabelWidth, mNoteLabelHeight);
-	addAndMakeVisible(&mFourthLowerNote);
-	mFourthLowerNote.setBounds(mHigherNoteX, mFourthRowY, mNoteLabelWidth, mNoteLabelHeight);
+	mFourthLowerNote.setBounds(mLowerNoteX, mFourthRowY, mNoteButtonWidth, mNoteButtonHeight);
+	mFourthLowerNote.setConnectedEdges(Button::ConnectedOnRight);
+
+	addAndMakeVisible(&mFourthHigherNote);
+	mFourthHigherNote.setBounds(mHigherNoteX, mFourthRowY, mNoteButtonWidth, mNoteButtonHeight);
+	mFourthHigherNote.setConnectedEdges(Button::ConnectedOnLeft);
+
 
 	// Setting the qualities positions
 	addAndMakeVisible(&mFirstQuality);
@@ -65,4 +78,53 @@ void QualitiesView::init()
 
 void QualitiesView::displayInstrument(InstrumentInfo &instrument)
 {
+	StringArray										qualities = instrument.getQualities();
+
+	std::vector<std::tuple<String, String, String>> parsedQualities;
+
+	// Dividing the qualities and the ranges into separate parts
+	for (auto &quality : qualities)
+	{
+		String lowerNote, higherNote, qualityText, range;
+
+		range		= splitColonizedStrings(quality, true);
+		qualityText = splitColonizedStrings(quality, false);
+		lowerNote	= getLowerOrHigherNote(range, true);
+		higherNote	= getLowerOrHigherNote(range, false);
+
+		parsedQualities.emplace_back(lowerNote, higherNote, qualityText);
+	}
+
+	TextButton *lowerNotes[]	  = {&mFirstLowerNote, &mSecondLowerNote, &mThirdLowerNote, &mFourthLowerNote};
+	TextButton *higherNotes[]	  = {&mFirstHigherNote, &mSecondHigherNote, &mThirdHigherNote, &mFourthHigherNote};
+	Label	   *qualitiesLabels[] = {&mFirstQuality, &mSecondQuality, &mThirdQuality, &mFourthQuality};
+
+	auto		configureLabel	  = [](Label &label, const String &text)
+	{
+		label.setVisible(!text.isEmpty());
+		label.setText(text, dontSendNotification);
+	};
+
+	auto configureButton = [](TextButton &button, const String &text)
+	{
+		button.setVisible(!text.isEmpty());
+		button.setButtonText(text);
+	};
+
+	for (size_t i = 0; i < std::size(lowerNotes); ++i)
+	{
+		if (i < parsedQualities.size())
+		{
+			const auto &[lowerNote, higherNote, qualityText] = parsedQualities[i];
+			configureButton(*lowerNotes[i], lowerNote);
+			configureButton(*higherNotes[i], higherNote);
+			configureLabel(*qualitiesLabels[i], qualityText);
+		}
+		else
+		{
+			lowerNotes[i]->setVisible(false);
+			higherNotes[i]->setVisible(false);
+			qualitiesLabels[i]->setVisible(false);
+		}
+	}
 }
