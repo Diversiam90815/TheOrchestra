918e8e933694b280c70359157bb4c5feea288465
Diversiam <jerry2912@icloud.com>
2024-12-08 19:13:37 +0100

    Key gets now stored in the InstrumentInfo

--- src/InstrumentInfo/InstrumentController.h
+++ src/InstrumentInfo/InstrumentController.h
@@ -22,25 +22,14 @@ public:
 	InstrumentController();
 	~InstrumentController();
 
-	/*
-	 @brief                 Returns the InstrumentInfo object of an instrument identified by a key
-	 @param                 [IN] key -> key of the instrument (see 'getInstrumentKey()')
-	 @return                InstrumentInfo object holding various information of the instrument
-	*/
-	InstrumentInfo getInstrument(int key);
 
+	InstrumentInfo getInstrument(int key);
 
 	bool		   loadFromJSON();
 
-private:
-	/*
-	 @brief                 Adding an instrument to the instruments map
-	 @param                 [IN] family -> enum: Family::Woodwinds (1), Family::Brass (2), Family::Strings (3), Family::Percussion (4)
-	 @param                 [IN] instrument -> enum values of the different instruments found in 'InstrumentInfo.h'
-	 @param                 [IN] InstrumentInfo object holding various information of the instrument
-	*/
-	void						  addInstrument(Family family, int instrumentId, const InstrumentInfo &info);
 
+private:
+	void						  addInstrument(Family family, int instrumentId, const InstrumentInfo &info);
 
 	StringArray					  readPlayingTechniquesFromJSON(DynamicObject *obj);
 
