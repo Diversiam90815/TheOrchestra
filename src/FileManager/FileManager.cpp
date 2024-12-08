d1895a81f2e0451c7445bf8745c78a9791b644d8
Diversiam <jerry2912@icloud.com>
2024-12-08 16:51:15 +0100

    Added method to get images from assets folder

--- src/FileManager/FileManager.cpp
+++ src/FileManager/FileManager.cpp
@@ -34,9 +34,18 @@ std::string FileManager::getInstrumentDataJSONPath()
 {
 	std::filesystem::path projectDir = getProjectDirectory();
 
-	std::filesystem::path dataDir = projectDir / AssetsFolderName / InstrumentDataFolderName / InstrumentsDataFileName;
+	std::filesystem::path dataDir	 = projectDir / AssetsFolderName / InstrumentDataFolderName / InstrumentsDataFileName;
 
 	return dataDir.string();
+}
+
+
+std::vector<std::string> FileManager::getInstrumentsImagesFolder(int instrumentKey)
+{
+	std::string familyName	   = getFamilyNameFromKey(instrumentKey);
+	std::string instrumentName = getInstrumentNameFromKey(instrumentKey);
+
+	return getInstrumentImages(familyName, instrumentName);
 }
 
 
@@ -45,4 +54,32 @@ std::filesystem::path FileManager::getProjectDirectory()
 	std::filesystem::path cwd		 = std::filesystem::current_path();
 	std::filesystem::path projectDir = cwd.parent_path();
 	return projectDir;
+}
+
+
+std::vector<std::string> FileManager::getInstrumentImages(const std::string &family, const std::string &instrumentName)
+{
+	std::filesystem::path	 projectDir = getProjectDirectory();
+	std::filesystem::path	 imagesDir	= projectDir / AssetsFolderName / ImageFolderName / family / instrumentName;
+
+	std::vector<std::string> images;
+	images.reserve(4); // there should be 3-4 images in the folder
+
+	if (std::filesystem::exists(imagesDir) && std::filesystem::is_directory(imagesDir))
+	{
+		for (const auto &entry : std::filesystem::directory_iterator(imagesDir))
+		{
+			if (entry.is_regular_file())
+			{
+				auto ext = entry.path().extension().string();
+				std::transform(ext.begin(), ext.end(), ext.begin(), ::tolower);
+				if (ext == ".png" || ext == ".jpg" || ext == ".jpeg")
+				{
+					images.push_back(entry.path().string());
+				}
+			}
+		}
+	}
+
+	return images;
 }
