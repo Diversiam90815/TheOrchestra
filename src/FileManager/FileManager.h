2b2c0ea4be479736ccb1d96e70d875401864fb8f
Diversiam <jerry2912@icloud.com>
2024-12-08 16:51:15 +0100

    Added method to get images from assets folder

--- src/FileManager/FileManager.h
+++ src/FileManager/FileManager.h
@@ -12,6 +12,9 @@
 #include "string"
 #include "filesystem"
 
+#include "Parameters.h"
+#include "Helper.h"
+
 
 class FileManager
 {
@@ -19,18 +22,25 @@ public:
 	FileManager();
 	~FileManager();
 
-	std::string getSamplesFolder();
+	std::string				 getSamplesFolder();
 
-	std::string getInstrumentDataJSONPath();
+	std::string				 getInstrumentDataJSONPath();
+
+	std::vector<std::string> getInstrumentsImagesFolder(int instrumentKey);
+
 
 private:
-
 	// Gets the project's directory
 	std::filesystem::path getProjectDirectory();
+
+	std::vector<std::string> getInstrumentImages(const std::string &family, const std::string &instrumentName);
+
 
 	std::string			  AssetsFolderName		   = "Assets";
 
 	std::string			  SampleFolderName		   = "Samples";
+
+	std::string			  ImageFolderName		   = "Images";
 
 	std::string			  InstrumentDataFolderName = "InstrumentData";
 
