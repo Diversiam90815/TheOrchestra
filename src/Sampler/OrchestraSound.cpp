9dcc19ac957f5dc87d9f40a49172ac39f7d12dfb
Jens W. Langenberg <jerry2912@icloud.com>
2025-01-13 21:58:45 +0100

    Added OrchestraSound/OrchestraVoice files to project

---
+++ src/Sampler/OrchestraSound.cpp
@@ -0,0 +1,10 @@
+/*
+  ==============================================================================
+
+	Module			OrchestraSound
+	Description		Custom sound class holding multiple dynamic layers & round-robins per single MIDI note/range
+
+  ==============================================================================
+*/ 
+
+#include "OrchestraSound.h"
