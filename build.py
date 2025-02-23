852a1be7977280540e6ed8236b116244d704646a
Jens W. Langenberg <jerry2912@icloud.com>
2025-02-23 11:42:08 +0100

    Updated build.py script

--- build.py
+++ build.py
@@ -7,9 +7,6 @@ import shutil
 from subprocess import Popen, PIPE, check_output
 
 
-PLATFORM_GENERATOR = '\"Visual Studio 17\"'
-
-
 class AutoCWD(object):
     """Auto restore directory"""
 
@@ -26,20 +23,16 @@ class BuildRunner(object):
     TARGET_CONFIG = 'Release'
 
     def __init__(self):   
-        formatter_class = lambda prog: argparse.HelpFormatter(prog, max_help_position=100, width=200)
-        description = 'This tool can be used to build and deploy the Instruments of the orchestra project.'
-        epilog = ''
-        parser = argparse.ArgumentParser(description=description, formatter_class=formatter_class, epilog=epilog)
+        parser = argparse.ArgumentParser(description='This tool can be used to build and deploy the "The Orchestra" project.')
         parser.add_argument('-p', '--prepare', action='store_true', help='prepares the project for use with IDE')
         parser.add_argument('-d', '--debug', action='store_true', help='prepare or build debug version')
         parser.add_argument('-b', '--build', action='store_true', help='build the project')
+        parser.add_argument('-v', '--version', action='store_true', help='display Python and CMake versions')
 
         self.args = parser.parse_args()
-       
+        self.platform = self._find_latest_visual_studio_version()
         self.version = ""
-
-        path_project = os.path.dirname(os.path.realpath(__file__))
-        self.args.path_project = path_project
+        self.args.path_project = os.path.dirname(os.path.realpath(__file__))
 
 
     @staticmethod
@@ -74,6 +67,37 @@ class BuildRunner(object):
         return result
 
 
+    def _find_latest_visual_studio_version(self):
+        vswhere = os.path.join(os.environ.get("ProgramFiles(x86)"), "Microsoft Visual Studio", "Installer", "vswhere.exe")
+
+        if not os.path.exists(vswhere):
+            print("vswhere.exe not found! Using Visual Studio 2022 as default")
+            return '\"Visual Studio 17\"'
+
+        result = check_output([vswhere, '-latest', '-property', 'installationVersion'], shell=True).decode().strip()
+        versionMatch = re.match(r'^(\d+)', result)
+
+        if versionMatch:
+            vsVersion = versionMatch.group(1)
+            return f'\"Visual Studio {vsVersion}\"'
+
+        else:
+            print("Could not determine latest Visual Studio version. Using Visual Studio 2022 as default")
+            return '\"Visual Studio 17\"'
+
+
+    def _print_versions(self):
+        """ Print Python, CMake & Projects versions """
+        self._update_app_version()
+        print(f"Project's version: {self.version}")
+        print(f"Python version: {sys.version}")
+        try:
+            cmake_version = self._execute_command("cmake --version", "Get CMake version")
+            print(f"CMake version: {cmake_version.splitlines()[0]}")  # print only the first line of output
+        except Exception as e:
+            print(f"Error retrieving CMake version: {e}")
+
+
     def _get_number_of_commits(self):
         autoCWD = AutoCWD(self.args.path_project)
         sys.stdout.flush()
@@ -100,7 +124,7 @@ class BuildRunner(object):
         return None
 
 
-    def _update_version_in_cmake(self, project_version):
+    def _update_app_version_in_cmake(self, project_version):
         pattern = r'set\(ORCHESTRA_VERSION\s+(\d+\.\d+\.\d+)\)'
         cmakeFile = os.path.join(self.args.path_project, "CMakeLists.txt")
         tmpFile = cmakeFile + '.tmp'
@@ -118,15 +142,15 @@ class BuildRunner(object):
         fileOut.close()
 
 
-    def _update_version(self):
-        BuildNumber = self._get_current_project_build_number()
+    def _update_app_version(self):
+        build_number = self._get_current_project_build_number()
         current_version = self._get_current_project_version()
 
         if current_version:
             version_parts = current_version.split('.')
-            version_parts[-1] = str(BuildNumber)
+            version_parts[-1] = str(build_number)
             self.version = '.'.join(version_parts)
-            self._update_version_in_cmake(self.version)
+            self._update_app_version_in_cmake(self.version)
             print(f"Set project version to {self.version}!")
 
         else:
@@ -137,9 +161,8 @@ class BuildRunner(object):
         projectfolderVS =  os.path.join(self.args.path_project)
         autoCWD = AutoCWD(projectfolderVS)
         
-        prepare_cmd = f'cmake -G {PLATFORM_GENERATOR} -B build -DCMAKE_TOOLCHAIN_FILE=build/build/generators/conan_toolchain.cmake'
-        prepare_cmd = f'cmake -G {PLATFORM_GENERATOR} -B build'
-        self._execute_command(prepare_cmd, "Select build generator")
+        prepare_cmd = f'cmake -G {self.platform} -B build'
+        self._execute_command(prepare_cmd, f"Select build generator : {self.platform}")
         
         del autoCWD
 
@@ -151,19 +174,21 @@ class BuildRunner(object):
         if os.path.exists(projectfolderVS + "/CMakeCache.txt"):
             self._execute_command("cmake --build " + projectfolderVS + " --target clean", "Run CMake clean")
         
-        self._execute_command("cmake --build " + buildFolder + " --config " + BuildRunner.TARGET_CONFIG + " --clean-first ", "Build Instruments of the Orchestra")    
+        self._execute_command("cmake --build " + buildFolder + " --config " + BuildRunner.TARGET_CONFIG + " --clean-first ", f"Build The Orchestra v{self.version}")    
 
 
         
     def doit(self):
+        if self.args.version:
+            self._print_versions()
         if self.args.debug:
             BuildRunner.TARGET_CONFIG = 'Debug'
         if self.args.prepare:
             self._build_prepare()
-            self._update_version()
+            self._update_app_version()
         if self.args.build:
             self._build_prepare()
-            self._update_version()
+            self._update_app_version()
             self._build_project()
 
             
