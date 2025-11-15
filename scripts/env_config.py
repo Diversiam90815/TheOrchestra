1e57a42afa37ad89ca39f9ffb3e032c55134802d
Jens Langenberg <jens.langenberg@corsair.com>
2025-11-15 20:05:02 +0100

    Adjusted script files

--- scripts/env_config.py
+++ scripts/env_config.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from .enums import Environment
 
 
-def update_environment_in_cmake(cmake_file: Path) -> Environment:
+def update_environment_in_cmake(cmake_file: Path, environment_var : str = "BUILD_ENVIRONMENT") -> Environment:
     branch_name = os.getenv("CI_COMMIT_REF_NAME", "") or ""
 
     if "release/" in branch_name:
@@ -15,7 +15,7 @@ def update_environment_in_cmake(cmake_file: Path) -> Environment:
     else:
         env = Environment.Development
 
-    pattern = re.compile(r"set\(\s*BUILD_ENVIRONMENT\s+(.*)\)")
+    pattern = re.compile(r"set\(\s*" + environment_var + "\s+(.*)\)")
 
     with cmake_file.open("r", encoding="utf-8") as f:
         lines = f.readlines()
@@ -23,7 +23,7 @@ def update_environment_in_cmake(cmake_file: Path) -> Environment:
     with cmake_file.open("w", encoding="utf-8") as f:
         for line in lines:
             if pattern.search(line):
-                f.write(f"set(BUILD_ENVIRONMENT {env.value})\n")
+                f.write(f"set({environment_var} {env.value})\n")
             else:
                 f.write(line)
 
