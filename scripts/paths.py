97ab6f9c8500838595ddbe501655a3774fd9e7eb
Jens Langenberg <jens.langenberg@corsair.com>
2025-11-16 12:04:25 +0100

    Removed -p/-b arguments for build script and added test only argument

--- scripts/paths.py
+++ scripts/paths.py
@@ -4,6 +4,7 @@ ROOT_DIR            = Path(__file__).resolve().parent.parent
 CMAKE_FILE          = ROOT_DIR / "CMakeLists.txt"
 BUILD_DIR           = ROOT_DIR / "build"
 CMAKE_INSTALL_DIR   = ROOT_DIR / "install"
+TEST_BUILD_DIR      = BUILD_DIR / "tests"
 
 
 def get_build_dir(architecture: str) -> Path:
