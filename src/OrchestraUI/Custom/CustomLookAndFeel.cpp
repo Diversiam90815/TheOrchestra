f2cb14b2cbd88f52b63321d7c42307ef79fac1a9
Jens Langenberg <jens.langenberg@corsair.com>
2025-11-16 18:24:50 +0100

    Included InstrumentData as binary data

--- src/OrchestraUI/Custom/CustomLookAndFeel.cpp
+++ src/OrchestraUI/Custom/CustomLookAndFeel.cpp
@@ -46,7 +46,7 @@ void CustomLookAndFeel::init()
 void CustomLookAndFeel::setFont()
 {
 	// Load the custom typeface from BinaryData
-	instrumentTypeface = juce::Typeface::createSystemTypefaceFor(BinaryData::InstrumentSerifRegular_ttf, BinaryData::InstrumentSerifRegular_ttfSize);
+	instrumentTypeface = juce::Typeface::createSystemTypefaceFor(FontData::InstrumentSerifRegular_ttf, FontData::InstrumentSerifRegular_ttfSize);
 
 	// Set the font
 	headerFont		   = juce::Font(instrumentTypeface).withHeight(27.0f);
