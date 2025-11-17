337b0ecde6f5bbe94729f4cd5dc26d8f9f6bfc06
Jens Langenberg <jens.langenberg@corsair.com>
2025-11-17 21:09:00 +0100

    Added basic wrapper for UserConfig (not yet impl)

---
+++ src/OrchestraCore/UserConfig/UserConfig.cpp
@@ -0,0 +1,35 @@
+/*
+  ==============================================================================
+	Module			User Config
+	Description		Managing the user configuration storing settings
+  ==============================================================================
+*/
+
+#include "UserConfig.h"
+
+
+std::string UserConfig::getSavedSamplesFolder()
+{
+	return std::string();
+}
+
+
+void UserConfig::saveSamplesFolder() {}
+
+
+bool UserConfig::loadConfigFile()
+{
+	return false;
+}
+
+
+bool UserConfig::saveConfigFile()
+{
+	return false;
+}
+
+
+std::string UserConfig::getConfigFullPath()
+{
+	return std::string();
+}
