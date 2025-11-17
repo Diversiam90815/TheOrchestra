ae461edb8ffc641e102a29816d41e4d10ee63782
Jens Langenberg <jens.langenberg@corsair.com>
2025-11-17 19:28:39 +0100

    Added settings menu for samples folder (just UI)

--- src/OrchestraUI/MenuBar/MenuBar.cpp
+++ src/OrchestraUI/MenuBar/MenuBar.cpp
@@ -10,7 +10,7 @@
 
 juce::StringArray MenuBar::getMenuBarNames()
 {
-	return {"Woodwinds", "Brass", "Strings", "Percussion"};
+	return {"Woodwinds", "Brass", "Strings", "Percussion", "Settings"};
 }
 
 
@@ -18,7 +18,7 @@ juce::PopupMenu MenuBar::getMenuForIndex(int topLevelMenuIndex, const juce::Stri
 {
 	juce::PopupMenu menu;
 
-	if (topLevelMenuIndex == 0) // Woodwinds
+	if (topLevelMenuIndex == MenuIndices::WoodwindsMenu)
 	{
 		menu.addItem(static_cast<int>(Woodwinds::Piccolo), PiccoloName);
 		menu.addItem(static_cast<int>(Woodwinds::Flute), FluteName);
@@ -29,7 +29,7 @@ juce::PopupMenu MenuBar::getMenuForIndex(int topLevelMenuIndex, const juce::Stri
 		menu.addItem(static_cast<int>(Woodwinds::Bassoon), BassoonName);
 		menu.addItem(static_cast<int>(Woodwinds::Contrabassoon), ContrabassoonName);
 	}
-	else if (topLevelMenuIndex == 1) // Brass
+	else if (topLevelMenuIndex == MenuIndices::BrassMenu)
 	{
 		menu.addItem(static_cast<int>(Brass::FrenchHorn), FrenchHornName);
 		menu.addItem(static_cast<int>(Brass::Trumpet), TrumpetName);
@@ -38,19 +38,23 @@ juce::PopupMenu MenuBar::getMenuForIndex(int topLevelMenuIndex, const juce::Stri
 		menu.addItem(static_cast<int>(Brass::Cimbasso), CimbassoName);
 		menu.addItem(static_cast<int>(Brass::Tuba), TubaName);
 	}
-	else if (topLevelMenuIndex == 2) // Strings
+	else if (topLevelMenuIndex == MenuIndices::StringsMenu)
 	{
 		menu.addItem(static_cast<int>(Strings::Violin), ViolinName);
 		menu.addItem(static_cast<int>(Strings::Viola), ViolaName);
 		menu.addItem(static_cast<int>(Strings::Violoncello), VioloncelloName);
 		menu.addItem(static_cast<int>(Strings::DoubleBass), DoubleBassName);
 	}
-	else if (topLevelMenuIndex == 3) // Percussion
+	else if (topLevelMenuIndex == MenuIndices::PercussionMenu)
 	{
 		menu.addItem(static_cast<int>(Percussion::Celeste), CelesteName);
 		menu.addItem(static_cast<int>(Percussion::Harp), HarpName);
 		menu.addItem(static_cast<int>(Percussion::Timpani), TimpaniName);
 		menu.addItem(static_cast<int>(Percussion::Marimba), MarimbaName);
+	}
+	else if (topLevelMenuIndex == MenuIndices::SettingsMenu)
+	{
+		menu.addItem(static_cast<int>(SettingsMenuItems::SetSamplesFolder), "Set samples folder..");
 	}
 
 	return menu;
@@ -59,6 +63,17 @@ juce::PopupMenu MenuBar::getMenuForIndex(int topLevelMenuIndex, const juce::Stri
 
 void MenuBar::menuItemSelected(int menuItemID, int topLevelMenuIndex)
 {
+	// Handle settings menu
+	if (topLevelMenuIndex == MenuIndices::SettingsMenu)
+	{
+		if (menuItemID == static_cast<int>(SettingsMenuItems::SetSamplesFolder))
+		{
+			showSampleFolderChooser();
+		}
+		return;
+	}
+
+	// Handle instrument selection
 	InstrumentID key = (topLevelMenuIndex + 1) * 100 + menuItemID;
 	LOG_INFO("Selected instrument with key {}", key);
 
@@ -72,4 +87,32 @@ void MenuBar::menuItemSelected(int menuItemID, int topLevelMenuIndex)
 void MenuBar::setInstrumentSelectedCallback(InstrumentSelectedCallback callback)
 {
 	mInstrumentSelectedCallback = callback;
+}
+
+
+void MenuBar::setSampleFolderChangedCallback(SampleFolderChangedCallback callback)
+{
+	mSamplesFolderChangedCallback = callback;
+}
+
+
+void MenuBar::showSampleFolderChooser()
+{
+	auto chooser = std::make_shared<juce::FileChooser>("Select Samples Folder", juce::File::getSpecialLocation(juce::File::userDocumentsDirectory), "", true);
+
+	auto flags	 = juce::FileBrowserComponent::openMode | juce::FileBrowserComponent::canSelectDirectories;
+
+	chooser->launchAsync(flags,
+						 [this, chooser](const juce::FileChooser &fc)
+						 {
+							 auto result = fc.getResult();
+
+							 if (result.isDirectory())
+							 {
+								 LOG_INFO("Samples folder set to: {}", result.getFullPathName().toStdString());
+
+								 if (mSamplesFolderChangedCallback)
+									 mSamplesFolderChangedCallback(result);
+							 }
+						 });
 }
--- src/OrchestraUI/MenuBar/MenuBar.h
+++ src/OrchestraUI/MenuBar/MenuBar.h
@@ -12,7 +12,8 @@
 #include "Logging.h"
 
 
-using InstrumentSelectedCallback = std::function<void(InstrumentID)>;
+using InstrumentSelectedCallback  = std::function<void(InstrumentID)>;
+using SampleFolderChangedCallback = std::function<void(const juce::File &)>;
 
 
 class MenuBar : public juce::MenuBarModel
@@ -26,9 +27,28 @@ public:
 	void			  menuItemSelected(int menuItemID, int topLevelMenuIndex) override;
 
 	void			  setInstrumentSelectedCallback(InstrumentSelectedCallback callback);
+	void			  setSampleFolderChangedCallback(SampleFolderChangedCallback callback);
 
 private:
-	juce::MenuBarComponent	   menuBar; // Menubar instance
+	void showSampleFolderChooser();
 
-	InstrumentSelectedCallback mInstrumentSelectedCallback;
+	enum MenuIndices
+	{
+		WoodwindsMenu  = 0,
+		BrassMenu	   = 1,
+		StringsMenu	   = 2,
+		PercussionMenu = 3,
+		SettingsMenu   = 4
+	};
+
+	// Settings menu item IDs
+	enum class SettingsMenuItems
+	{
+		SetSamplesFolder = 1
+	};
+
+	juce::MenuBarComponent		menuBar; // Menubar instance
+
+	InstrumentSelectedCallback	mInstrumentSelectedCallback;
+	SampleFolderChangedCallback mSamplesFolderChangedCallback;
 };
