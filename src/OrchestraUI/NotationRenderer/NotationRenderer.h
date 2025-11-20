931220cc5195384c8d761008104ed4dca3f5d378
Jens Langenberg <jens.langenberg@corsair.com>
2025-11-20 21:24:42 +0100

    Added basic noterenderer class files

---
+++ src/OrchestraUI/NotationRenderer/NotationRenderer.h
@@ -0,0 +1,33 @@
+/*
+  ==============================================================================
+	Module			NotationRenderer
+	Description		Renders musical notation
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "JuceIncludes.h"
+
+enum class Clef
+{
+	Treble,
+	Bass,
+	Alto,
+	Tenor,
+};
+
+enum class Accidental
+{
+	None,
+	Sharp,
+	Flat,
+	Natural,
+	DoubleSharp,
+	DoubleFlat,
+};
+
+
+class NotationRenderer
+{
+};
