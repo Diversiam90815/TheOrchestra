a2cf4c72641c5053f8521abd4895dc0dac70127d
Jens Langenberg <william@diversiam.com>
2026-08-15 21:30:03 +0200

    Fixed issues in CMake modules

--- cmake/CppCheck.cmake
+++ cmake/CppCheck.cmake
@@ -1,5 +1,11 @@
 function(AddCppCheck target)
-  find_program(CPPCHECK_PATH cppcheck REQUIRED)
+  find_program(CPPCHECK_PATH cppcheck)
+
+  if(NOT CPPCHECK_PATH)
+    message(WARNING "ENABLE_CPPCHECK is ON but cppcheck was not found on PATH - skipping static analysis for ${target}.")
+    return()
+  endif()
+
   set_target_properties(${target}
     PROPERTIES CXX_CPPCHECK
     "${CPPCHECK_PATH};--enable=warning;--error-exitcode=1"
